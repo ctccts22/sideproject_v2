e6ba455e51bfe23bbfd6219b2f9f228344be59b4
ctccts22 <ctccts22@gmail.com>
2024-07-26 17:40:41 +0900

    # writing test code

--- backend/src/test/java/v2/sideproject/store/users/service/AuthServiceTest.java
+++ backend/src/test/java/v2/sideproject/store/users/service/AuthServiceTest.java
@@ -1,4 +1,9 @@
 package v2.sideproject.store.users.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
+
 }
