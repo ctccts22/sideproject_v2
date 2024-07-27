15bc528dedc4f47f35f48117b516202255da014e
ctccts22 <ctccts22@gmail.com>
2024-07-28 01:39:10 +0900

    # Writing test code

---
+++ backend/src/main/java/v2/sideproject/store/user/models/vo/response/UsersOneDetailResponse.java
@@ -0,0 +1,35 @@
+package v2.sideproject.store.user.models.vo.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import v2.sideproject.store.user.models.enums.MobileCarrier;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class UsersOneDetailResponse {
+
+    @Email
+    @NotEmpty(message = "Email cannot be null or empty")
+    @Schema(description = "Email is used for login", example = "test@test.com")
+    private String email;
+
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Schema(description = "User's name", example = "John Doe")
+    private String name;
+
+    @NotNull(message = "Status cannot be null")
+    @Schema(description = "User's mobileCarrier", example = "KT")
+    private MobileCarrier mobileCarrier;
+
+    @NotEmpty(message = "Cellphone number cannot be null or empty")
+    @Schema(description = "User's phone number", example = "123-456-7890")
+    private String phone;
+}
