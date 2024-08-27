b70215a53384a242c1a7cf6e174bb4bc5a88baa7
ctccts22 <ctccts22@gmail.com>
2024-08-27 17:49:38 +0900

    # react instance

--- frontend/src/plugin/axios.js
+++ frontend/src/plugin/axios.js
@@ -1,4 +1,3 @@
-import useAuthStore from '../stores/auth/authStore.js';
 import axios from 'axios';
 
 const API_DEV_URL = 'http://localhost:8080';
@@ -7,41 +6,17 @@ const instance = axios.create({
   baseURL: API_DEV_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
-// Request interceptor
-
-instance.interceptors.request.use(async (config) => {
-  const { setUserInfo, setIsAuthenticated, setLoading } = useAuthStore.getState();
-
-  if (config.url === '/api/auth/login') {
-    return config; // Skip any refresh logic during login
+// Optionally, you can keep other interceptors for handling request/response globally
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    // Handle errors
+    return Promise.reject(error);
   }
+);
 
-  if (config.url !== '/api/auth/refresh') {
-    try {
-      console.log('Attempting to refresh token.');
-
-      // Call the refresh endpoint
-      const response = await instance.get('/api/auth/refresh', {
-        withCredentials: true
-      });
-
-      console.log(response.data);
-      const { email, role } = response.data;
-
-      setUserInfo(email, role);
-      setIsAuthenticated(true);
-    } catch (error) {
-      console.error('Failed to refresh token:', error.message);
-      setIsAuthenticated(false);
-    }
-  }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
 export default instance;
